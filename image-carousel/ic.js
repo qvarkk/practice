0be76904a09c4b83d5ba9d60e37c3b959232ccca
qvarkk <xumdes@gmail.com>
2022-10-21 10:30:01 +0500

    something

---
+++ image-carousel/ic.js
@@ -0,0 +1,7 @@
+const imageHolder = document.querySelector("#imageHolder");
+const nextImageBtn = document.querySelector("#nextImageBtn");
+const prevImageBtn = document.querySelector("#prevImageBtn");
+
+nextImageBtn.addEventListener("click", () => {});
+
+function ScrollImageToNext(speed) {}
